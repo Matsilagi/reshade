13d2c5b227fe0876d6c24a7badeb0fbcb10eaea9
Matheus Silagi <matsilagi2@hotmail.com>
2019-05-27 14:27:15 -0300

    Removed network verification
    Where we are going, we won't need those.

--- source/opengl/runtime_opengl.cpp
+++ source/opengl/runtime_opengl.cpp
@@ -1159,11 +1159,11 @@ void reshade::opengl::runtime_opengl::detect_depth_source()
 
 	if (_has_high_network_activity)
 	{
-		_depth_source = 0;
-		glDeleteTextures(1, &_tex[TEX_DEPTH]);
-		_tex[TEX_DEPTH] = 0;
-		update_texture_references(texture_reference::depth_buffer);
-		return;
+		//_depth_source = 0;
+		//glDeleteTextures(1, &_tex[TEX_DEPTH]);
+		//_tex[TEX_DEPTH] = 0;
+		//update_texture_references(texture_reference::depth_buffer);
+		//return;
 	}
 
 	assert(_app_state.has_state);
